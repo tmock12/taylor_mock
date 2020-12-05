eb6f59cb516613cb5e0ed738c4a1451c389a0383
Taylor Mock <tmock12@gmail.com>
2020-12-05 13:12:19 -0600

    Add user authentication

---
+++ lib/taylor_mock/accounts/user_token.ex
@@ -0,0 +1,61 @@
+defmodule TaylorMock.Accounts.UserToken do
+  use Ecto.Schema
+  import Ecto.Query
+
+  @rand_size 32
+  @session_validity_in_days 60
+
+  @primary_key {:id, :binary_id, autogenerate: false, read_after_writes: true}
+  @foreign_key_type :binary_id
+  schema "users_tokens" do
+    field :token, :binary
+    field :context, :string
+    belongs_to :user, TaylorMock.Accounts.User
+
+    timestamps(updated_at: false)
+  end
+
+  @doc """
+  Generates a token that will be stored in a signed place,
+  such as session or cookie. As they are signed, those
+  tokens do not need to be hashed.
+  """
+  def build_session_token(user) do
+    token = :crypto.strong_rand_bytes(@rand_size)
+    {token, %TaylorMock.Accounts.UserToken{token: token, context: "session", user_id: user.id}}
+  end
+
+  @doc """
+  Checks if the token is valid and returns its underlying lookup query.
+
+  The query returns the user found by the token.
+  """
+  def verify_session_token_query(token) do
+    query =
+      from token in token_and_context_query(token, "session"),
+        join: user in assoc(token, :user),
+        where: token.inserted_at > ago(@session_validity_in_days, "day"),
+        select: user
+
+    {:ok, query}
+  end
+
+  @doc """
+  Returns the given token with the given context.
+  """
+  def token_and_context_query(token, context) do
+    from TaylorMock.Accounts.UserToken, where: [token: ^token, context: ^context]
+  end
+
+  @doc """
+  Gets all tokens for the given user for the given contexts.
+  """
+  def user_and_contexts_query(user, :all) do
+    from t in TaylorMock.Accounts.UserToken, where: t.user_id == ^user.id
+  end
+
+  def user_and_contexts_query(user, [_ | _] = contexts) do
+    from t in TaylorMock.Accounts.UserToken,
+      where: t.user_id == ^user.id and t.context in ^contexts
+  end
+end
