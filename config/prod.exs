3ae97d1802646265d0a5b290da1caa5279fe249e
Taylor Mock <tmock12@gmail.com>
2021-03-16 20:30:00 -0500

    Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :taylor_mock, TaylorMockWeb.Endpoint,
-  url: [host: System.get_env("HOST"), port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: {:system, "HOST"}, port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
