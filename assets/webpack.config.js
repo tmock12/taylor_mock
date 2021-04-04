cadebf0458668d9a47bbc2217d3565eaea4b5a66
Taylor Mock <tmock12@gmail.com>
2021-04-03 19:54:26 -0500

    Bump webpack

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,38 +1,29 @@
 const path = require('path');
 const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = (env, options) => {
   const devMode = options.mode !== 'production';
 
   return {
-    optimization: {
-      minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    },
     entry: {
-      'app': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+      app: glob.sync('./vendor/**/*.js').concat(['./js/app.js']),
     },
     output: {
-      filename: '[name].js',
       path: path.resolve(__dirname, '../priv/static/js'),
-      publicPath: '/js/'
+      filename: '[name].js',
+      publicPath: '/js/',
     },
-    devtool: devMode ? 'eval-cheap-module-source-map' : undefined,
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: 'babel-loader',
+          },
         },
         {
           test: /\.[s]?css$/,
@@ -42,13 +33,18 @@ module.exports = (env, options) => {
             'sass-loader',
             'postcss-loader',
           ],
-        }
-      ]
+        },
+      ],
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-    ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
-  }
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'static/', to: '../' }],
+      }),
+    ],
+    optimization: {
+      minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool: devMode ? 'source-map' : undefined,
+  };
 };
