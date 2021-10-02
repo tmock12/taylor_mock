30055fef1199403e273c28b5df2af4a450c81e39
Taylor Mock <tmock12@gmail.com>
2021-10-02 12:42:30 -0500

    Use heex for html

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule TaylorMock.MixProject do
       {:phoenix_html, "~> 3.0.4"},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
-      {:phoenix_live_view, "~> 0.16.0"},
+      {:phoenix_live_view, "~> 0.16.4"},
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
       {:telemetry_metrics, "~> 0.6"},
