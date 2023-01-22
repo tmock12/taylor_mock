45e2d64876c69a777fcbf49b9b88d7cdd4c10c2e
Taylor Mock <tmock12@gmail.com>
2023-01-22 14:23:34 -0600

    Bump elixir and erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TaylorMock.MixProject do
     [
       app: :taylor_mock,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
