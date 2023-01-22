0ea2e84ce7037d5615d644744e272ee5c48c9c7b
Taylor Mock <tmock12@gmail.com>
2023-01-22 16:06:45 -0600

    Bump liveview

--- lib/taylor_mock_web/live/til_live/show.ex
+++ lib/taylor_mock_web/live/til_live/show.ex
@@ -26,7 +26,7 @@ defmodule TaylorMockWeb.TilLive.Show do
     <div class="max-w-5xl px-6 mx-auto">
       <h1 class="text-2xl font-semibold text-center text-gray-800">Today I Learned</h1>
       <div class="flex flex-col items-center justify-center mt-6">
-        <%= live_component(@socket, TaylorMockWeb.Live.TilLive.PostComponent, post: @post) %>
+        <%= live_component(TaylorMockWeb.Live.TilLive.PostComponent, post: @post) %>
       </div>
     </div>
     """
--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule TaylorMock.MixProject do
       {:phoenix, "~> 1.6.0"},
       {:phoenix_html, "~> 3.1.0"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
-      {:phoenix_live_view, "~> 0.16.4"},
+      {:phoenix_live_view, "~> 0.17.12"},
       {:plug_cowboy, "~> 2.0"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 0.5"}
